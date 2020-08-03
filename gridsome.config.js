44eb667d6edc24749cbcd567d724ac23a4a21c7e
John Quealy <john@urbildpunkt.com>
2020-08-03 11:26:10 -0700

    Update prismjs config

--- gridsome.config.js
+++ gridsome.config.js
@@ -70,10 +70,30 @@ module.exports = {
       options: {
         tailwindConfig: './tailwind.config.js',
         purgeConfig: {
-          // Prevent purging of prism classes.
-          whitelistPatternsChildren: [
-            /token$/
-          ]
+          content: [
+            './src/**/*.vue',
+            './src/**/*.js',
+            './src/**/*.jsx',
+            './src/**/*.html',
+            './src/**/*.pug',
+            './src/**/*.md',
+          ],
+          whitelist: [
+              'body',
+              'html',
+              'img',
+              'a',
+              'g-image',
+              'g-image--lazy',
+              'g-image--loaded',
+          ],
+          whitelistPatternsChildren: [/token$/],
+          extractors: [
+              {
+                  extractor: content => content.match(/[A-z0-9-:\\/]+/g),
+                  extensions: ['vue', 'js', 'jsx', 'md', 'html', 'pug'],
+              },
+          ],
         }
       }
     },
